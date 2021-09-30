f052878f3aaea16218111766a4ba007d4e1f5803
Makarand Ratnakar <makarand.ratnakar11@gmail.com>
2021-10-01 00:59:01 +0530

    1st commit

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt-get install libsdl-image1.2 libsdl-image1.2-dev guile-2.0 
+sudo apt-get install guile-2.0-dev libsdl1.2debian libart-2.0-dev libaudiofile-dev 
+sudo apt-get install libesd0-dev libdirectfb-dev libdirectfb-extra libfreetype6-dev 
+sudo apt-get install libxext-dev x11proto-xext-dev libfreetype6 libaa1 libaa1-dev 
+sudo apt-get install libslang2-dev libasound2 libasound2-dev
+sudo apt install libplib-dev
+
+sudo apt-get install libqt4-dev
+sudo apt-get install libjack0
+sudo apt-get install libjack-dev
+sudo apt-get install libasound2-dev
+sudo apt-get install libsndfile1-dev
